6f3eb967a5c8d81968ba604c126d20248a4d1481
SmileyDoodle <smileydoodle.official@gmail.com>
2020-07-18 12:43:47 +0300

    "To-Do-List" - version 2 . Added Mongoose

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -9,19 +10,50 @@ app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
-const tasks = [];
+mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const taskSchema = {
+    name: {
+      type: String,
+      required: [true, 'Fill in the form!']
+    },
+};
+
+const Task = mongoose.model('Task', taskSchema);
+
+// const task1 = new Task({
+//     name: "Welcome"
+// });
+
+// const defaultTasks = [task1];
+
+// Task.insertMany(defaultTasks, function(err) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("success")
+//     }
+// });
 
 app.get("/", function(req, res) {
 
     const day = date.getDate();
 
-    res.render('list', {kindOfDay: day, tasks: tasks}); 
+    Task.find({}, function(err, tasks) {
+        res.render('list', {kindOfDay: day, tasks: tasks}); 
+    });
+
 });
 
 app.post("/", function (req, res) {
 
     const newTask = req.body.inputTask;
-    tasks.push(newTask);
+
+    const task = new Task({
+        name: newTask
+    });
+
+    task.save();
 
     res.redirect("/");
 });
@@ -29,7 +61,10 @@ app.post("/", function (req, res) {
 app.post("/delete", function (req, res) {
 
     const taskIndex = req.body.index;
-    tasks.splice(taskIndex, 1);
+
+    Task.deleteOne({ _id: taskIndex }, function (err) {
+        if (err) return handleError(err);
+      });
 
     res.redirect("/");
 });
@@ -46,4 +81,4 @@ app.listen(3000, function() {
 // var today = new Date();
 // var currentDay = today.getDay()
 // var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-// var day = daysOfTheWeek[currentDay];;
+// var day = daysOfTheWeek[currentDay];; 
